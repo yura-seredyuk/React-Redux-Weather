344c80781bc88fe63e75723fcae79c18c90c68a4
Yurii Seredyuk <yura2590@gmail.com>
2021-01-20 03:08:56 +0200

    Load start project

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, {Fragment} from 'react';
+import ReactDOM, { render } from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import Weather from './components/weather'
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+class App extends React.Component{
+
+  render(){
+    return(
+      <Fragment>
+        <Weather />
+      </Fragment>
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
